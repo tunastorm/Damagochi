e7ea556d75cbe2dda3540fdb2ebeefc6e5cd9e2e
tunastorm <ycw921@naver.com>
2024-06-06 14:07:13 +0900

    Initial Commit

---
+++ Damagochi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Damagochi
+//
+//  Created by 유철원 on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
